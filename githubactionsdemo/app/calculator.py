e49ef47011bccfcaede116ee0f028e0d2e57fa5a
deepikaamehta <deepika@mac.home>
2025-06-26 22:49:02 -0400

    Fix: correct syntax and spacing for add() function

--- githubactionsdemo/app/calculator.py
+++ githubactionsdemo/app/calculator.py
@@ -1,7 +1,5 @@
-# If this is the very top of the file, just add two blank lines before the function
 
-# <- 1st blank line
-
-# <- 2nd blank line
+# <- Blank line
+# <- Blank line
 def add(a, b):
     return a + b
